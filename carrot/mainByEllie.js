5ceef7ecba329ddcd422c5bb4499d3941789275a
stillgood96 <a787574@naver.com>
2021-04-23 02:50:16 +0900

    completed

--- carrot/mainByEllie.js
+++ carrot/mainByEllie.js
@@ -4,6 +4,7 @@
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
@@ -11,9 +12,22 @@ const gameBtn = document.querySelector(".game__button");
 const gameTimer = document.querySelector(".game__timmer");
 const gameScore = document.querySelector(".game__score");
 
+const popUp = document.querySelector(".pop-up");
+const popUpText = document.querySelector(".pop-up__message");
+const popUpRefresh = document.querySelector(".pop-up__refresh");
+
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const alertSound = new Audio("./sound/alert.wav");
+const bgSound = new Audio("./sound/bg.mp3");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const winSound = new Audio("./sound/game_win.mp3");
+
 let started = false;
 let score = 0;
 let timer = undefined;
+
+//onFieldClick => (event) => onFieldClick(event); 생략ver
+field.addEventListener("click", onFieldClick);
 
 gameBtn.addEventListener("click", () => {
   console.log("btn Clicked");
@@ -22,22 +36,52 @@ gameBtn.addEventListener("click", () => {
   } else {
     startGame();
   }
-  started = !started;
+});
+
+popUpRefresh.addEventListener("click", () => {
+  startGame();
+  hidePopUp();
 });
 
 function startGame() {
+  started = true;
   initGame();
   showStopButton();
   showTimerAndScore();
-  startGameTime();
+  startGameTimer();
+  playSound(bgSound);
 }
 
-function stopGame() {}
+function stopGame() {
+  started = false;
+  stopGameTimer();
+  hideGameButton();
+  showPopUpWithText("REPLAY?");
+  playSound(alertSound);
+  stopSound(bgSound);
+}
 
+function finishGame(win) {
+  started = false;
+  hideGameButton();
+  if (win) {
+    playSound(winSound);
+  } else {
+    playSound(bugSound);
+  }
+  stopGameTimer();
+  stopSound(bgSound);
+  showPopUpWithText(win ? "You Won" : "You Lost");
+}
+
+function hideGameButton() {
+  gameBtn.style.visibility = "hidden";
+}
 function showStopButton() {
-  const icon = gameBtn.querySelector(".fa-play");
+  const icon = gameBtn.querySelector(".fas");
   icon.classList.add("fa-stop");
-  icon.classList.remove("fa-ply");
+  icon.classList.remove("fa-play");
+  gameBtn.style.visibility = "visible";
 }
 
 function showTimerAndScore() {
@@ -45,7 +89,40 @@ function showTimerAndScore() {
   gameScore.style.visibility = "visible";
 }
 
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      finishGame(CARROT_COUNT === score);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes} : ${seconds}`;
+}
+
+function showPopUpWithText(text) {
+  popUpText.innerText = text;
+  popUp.classList.remove("pop-up--hide");
+}
+
+function hidePopUp() {
+  popUp.classList.add("pop-up--hide");
+}
+
 function initGame() {
+  score = 0;
   field.innerHTML = "";
   gameScore.innerText = CARROT_COUNT;
   // 벌레와 당근을 생성한뒤 field에 추가해줌
@@ -54,6 +131,41 @@ function initGame() {
   addItem("bug", 5, "img/bug.png");
 }
 
+// 게임이 시작하지 않았을 때 이벤트가 발생하면 안된다.
+// 이 때 함수 내부에서 빠른 리턴처리가 중요 !
+// matches함수는 css 클래스와 확인여부를
+function onFieldClick(event) {
+  if (!started) return;
+
+  const target = event.target;
+  if (target.matches(".carrot")) {
+    // 당근!!
+    target.remove();
+    score++;
+    playSound(carrotSound);
+    updateScoreBoard();
+
+    if (score === CARROT_COUNT) {
+      finishGame(true);
+    }
+  } else if (target.matches(".bug")) {
+    // 벌레 !!
+    finishGame(false);
+  }
+}
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function stopSound(sound) {
+  sound.pause();
+}
+
+function updateScoreBoard() {
+  gameScore.innerText = CARROT_COUNT - score;
+}
 // Field 내에 랜덤한 위치에 요소 깔기
 // x1, y1에 field의 width, height의 시작 값인 0 을 입력
 // x2, y2에는 field의 MAX_width, MAX_height값을 입력 후
